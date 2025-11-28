84539d82dd6e15e6b3848ec8ed6b309e561fb3cd
ilhandahir1122-debug <ilhandahir1122@gmail.com>
2025-11-28 21:21:47 +0300

    Create config.jsx

---
+++ src/config.jsx
@@ -0,0 +1,5 @@
+export const RPC_URL = "http://127.0.0.1:8545";
+export const CHAIN_NAME = "Luwa Chain";
+export const SYMBOL = "LUWA";
+export const BLOCK_TIME = "5 seconds";
+export const BLOCK_REWARD = "5 LUWA";
